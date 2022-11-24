fee6914d7b95feb012c783e8377e281ceedacfd2
Kyle Beasley <kyle.beasley22@gmail.com>
2022-11-23 23:19:55 -0600

    Added footer

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,10 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+    path: path.resolve(__dirname, 'dist')
+    },
+    watch: true,
+
   module: {
     rules: [
       {
